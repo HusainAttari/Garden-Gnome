c44430825b1ba14b4edb73cecaa21bf513730eed
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-07-09 13:23:03 +0530

    Create reset.js

---
+++ commands/auctions/reset.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const info = require('../../auction.js')
+
+module.exports = {
+	commands : 'reset',
+  expectedArgs : '',
+  permissionError : 'You do not have permission to run this command.',
+  minArgs : 0,
+  maxArgs : 0,
+  permissions : [],
+  requiredRoles : ['Auctioneer'],
+	requiredId : [],
+  callback : async (message, arguments, text) => {
+		await info.resetAuc(message.channel.id)
+		message.channel.send('The ongoing auction has been successfully reset.')
+	},
+}
