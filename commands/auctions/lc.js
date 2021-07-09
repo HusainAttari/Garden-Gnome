aa97990b2b973506920903093829782aebaecd76
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-07-09 17:48:16 +0530

    Update lc.js

--- commands/auctions/lc.js
+++ commands/auctions/lc.js
@@ -5,39 +5,50 @@ module.exports = {
 	commands : ['lc', 'lastcall'],
   expectedArgs : '<Last call pings separated with a space>',
   permissionError : 'You do not have permission to run this command.',
-  minArgs : 1,
+  minArgs : 0,
   maxArgs : null,
   permissions : [],
   requiredRoles : ['Auctioneer'],
 	requiredId : [],
   callback : async (message, arguments, text) => {
 		
+	  	let x = await info.getAuc(message.channel.id)
 		let lc = ''
-
-		for (var arg of arguments){
-			arg = arg.toLowerCase()
-			switch (arg){
-				case 'pog' : 
-					lc = lc+ '<@&838115681689010197> '
-					break
-				case 'legendary' :
-					lc = lc + '<@&825256942791819264> '
-					break
-				case 'shiny' :
-					lc = lc + '<@&825256996437229568> '
-					break
-				case 'lr' :
-					lc = lc + '<@&825257678447706152> '
-					break
-				case 'mega' :
-					lc = lc + '<@&825257756008382475> '
-					break
+		if (arguments){
+			for (var arg of arguments){
+				arg = arg.toLowerCase()
+				switch (arg){
+					case 'pog' : 
+						lc = lc+ '<@&838115681689010197> '
+						break
+					case 'legendary' :
+						lc = lc + '<@&825256942791819264> '
+						break
+					case 'shiny' :
+						lc = lc + '<@&825256996437229568> '
+						break
+					case 'lr' :
+						lc = lc + '<@&825257678447706152> '
+						break
+					case 'mega' :
+						lc = lc + '<@&825257756008382475> '
+						break
+				}
 			}
-		}
-
-		if(!lc){
-			message.channel.send('Please put the last calls you want <pog/legendary/shiny/lr/mega>')
-			return
+		}else{
+			if (x.lc != '<@&825233232341106738>'){
+				const embed = new Discord.MessageEmbed()
+				.setColor('#aff0ae')
+				.setFooter('Venture Auction Gardens')
+				.setDescription(`**Last Call Pings :**\n${lc}`)
+				message.channel.send(embed)
+			}else{
+				const embed = new Discord.MessageEmbed()
+				.setDescription('No last call pings set. The <@&825233232341106738> role will be pinged.')
+				.setColor('#aff0ae')
+				.setFooter('Venture Auction Gardens')
+				message.channel.send(embed)
+			}
 		}
 
 		message.channel.send('Last call pings set!')
