ef03afa9e23cbfef7e7d9465e01a6d4cd34a2728
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-07-09 13:14:57 +0530

    Create auction-schema.js

---
+++ schemas/auction-schema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const reqString = {
+	type : String,
+	required : true
+}
+
+const auctionSchema = mongoose.Schema({
+	_id : reqString,
+	channelId : reqString,
+	seller : reqString,
+	sellerId : reqString,
+	bidder : {type : String, required : true, default : "No highest bidder"},
+	pokemon : reqString,
+	level : {type : Number, required : true},
+	rarity : reqString,
+	nature : reqString,
+	mints : {type : Number, required : true, default : 0},
+	ability : reqString,
+	auto : {type : String, required : false, default : 'N/A'},
+	time : {type : Number, required : true},
+	offer : {type : Number, required : true, default : 0},
+	acceptedPay : {type : String, required : true, default : "Coins only"},
+	img : reqString,
+	bundle : {type : Boolean, required : true, default : false},
+	lc : {type : String}
+})
+
+module.exports = mongoose.model('Auctions', auctionSchema)
