ca2f8bb07f27666c10c628ef04802f5273ba884d
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-07-12 10:54:56 +0530

    Update help.js

--- commands/auctions/help.js
+++ commands/auctions/help.js
@@ -59,7 +59,7 @@ module.exports = {
       const embed = new Discord.MessageEmbed()
       .setColor('#aff0ae')
       .setFooter('Created by @Diablo#1234')
-      .setDescription('[Join here to support the creator](https://discord.gg/QQ5N4uspdK)\n[Click here for VAG Server link](https://discord.gg/tzfehaGZ2H)\n\nUse `!help <command>` to get more information about a command.\n\n**List of all available commands :**\n\n`!auc` - To start an auction\n`!bid` - To bid on an ongoing auction\n`!status` - To view the status of an ongoing auction\n`!lc` - To set last call pings. (Auctioneer only)\n`!or` - To overwrite an auction. (Auctioneer only)\n`!reset` - To reset an ongoing auction. (Auctioneer only)')
+      .setDescription('[Join here to support the creator](https://discord.gg/QQ5N4uspdK)\n[Click here for VAG Server link](https://discord.gg/tzfehaGZ2H)\n\nUse `!help <command>` to get more information about a command.\n\n**List of all available commands :**\n\n`!auc` - To start an auction\n`!bal` - To bid on an ongoing auction\n`!status` - To view the status of an ongoing auction\n`!lc` - To set last call pings. (Auctioneer only)\n`!or` - To overwrite an auction. (Auctioneer only)\n`!reset` - To reset an ongoing auction. (Auctioneer only)')
       /*.addFields({
           name : '`!auc`',
           value : 'To start an auction'
