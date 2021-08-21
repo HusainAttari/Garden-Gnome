1183a9585b91ed7c68797df2eb0cbf8e444e78e4
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-08-21 19:50:29 +0530

    Update test.js

--- commands/auctions/test.js
+++ commands/auctions/test.js
@@ -11,7 +11,7 @@ module.exports = {
 	requiredId : ['819620327235387408'],
   callback : (message, arguments, text) => {
     if (arguments[0] === 'embedinfo' || arguments[0] === 'ei'){
-      message.channel.awaitMessages(m => m.author.id === '666956518511345684' && message.embeds[0], {max: 1, time: 30000, error: ['time']}).then(col => {
+      message.channel.awaitMessages(m => m.author.id === '666956518511345684' && m.embeds[0], {max: 1, time: 30000, error: ['time']}).then(col => {
         console.log(col.first().embeds[0])
       }).catch(err => console.log(err))
     }
