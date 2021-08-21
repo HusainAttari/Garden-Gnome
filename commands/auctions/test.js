e075b3f65a3780f4dcff34eb7e485c46a95b7c04
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-08-21 19:45:01 +0530

    Create test.js

---
+++ commands/auctions/test.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+
+module.exports = {
+	commands : 'test',
+  expectedArgs : '',
+  permissionError : 'You do not have permission to run this command.',
+  minArgs : 0,
+  maxArgs : null,
+  permissions : [],
+  requiredRoles : [],
+	requiredId : ['819620327235387408'],
+  callback : (message, arguments, text) => {
+    if (arguments[0] === 'embedinfo' || arguments[0] === 'ei'){
+      message.channel.awaitMessages(m => m.author.id === '666956518511345684' && message.embeds[0], {max: 1, time: 30000, error: ['time']}).then(col => {
+        console.log(col.first().embeds[0])
+      }.catch(err => console.log(err))
+    }
+	},
+}
