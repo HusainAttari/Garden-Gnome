15554b1e932282c4ea8844ab792d28e8cf73fdfe
HusainAttari <78153344+HusainAttari@users.noreply.github.com>
2021-09-02 16:11:52 +0530

    Update test.js

--- commands/auctions/test.js
+++ commands/auctions/test.js
@@ -12,7 +12,7 @@ module.exports = {
   callback : (message, arguments, text) => {
     if (arguments[0] === 'embedinfo' || arguments[0] === 'ei'){
       message.channel.awaitMessages(m => m.author.id === '666956518511345684' && m.embeds[0], {max: 1, time: 30000, error: ['time']}).then(col => {
-        console.log(col.first().embeds[0])
+        console.log(col.first())
 	message.channel.send(`Embed recorded!`)
       }).catch(err => console.log(err))
     }
